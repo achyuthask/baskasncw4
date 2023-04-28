63e42f77563c76643c3be202285947020b4af03c
achyuthask <96536973+achyuthask@users.noreply.github.com>
2023-04-29 00:49:34 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,27 @@
-import logo from './logo.svg';
-import './App.css';
+
+
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+     <h1>B.Achyutha sri kashyap </h1>
+     <p>learning at geekster</p>
+     <a href=" ">linkdin </a> <hr></hr>
+     <h1>Expierice</h1>
+     <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Possimus, ullam! Maxime officiis consequuntur libero eaque reprehenderit quisquam nam ab vitae totam id commodi laborum enim, sint ea odit quasi soluta!
+Lorem ipsum dolor sit, amet consectetur adipisicing elit. Provident iusto ut praesentium modi. Incidunt atque voluptatem, dignissimos officiis reprehenderit est!</p> <hr></hr>
+
+ <h1>Skills</h1>
+ <li>java</li>
+ <li>javascript</li>
+ <li>html</li>
+ <li>css</li>
+ <li>react js</li> <hr></hr>
+ <h1>Hobbies</h1>
+ <li>anime</li>
+ <li>
+  movies
+ </li>
     </div>
   );
 }
